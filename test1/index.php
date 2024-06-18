7bdec9a0a11580a98e146c7661452e75cf3abe3b
Bohdan Maksymchuk <WinK32@users.noreply.github.com>
2024-06-18 12:37:37 +0300

    Update index.php

--- test1/index.php
+++ test1/index.php
@@ -35,10 +35,10 @@ $bug = new Bug();
         Report a Bug
     </button>
     <div id="bug-form" class="hidden">
-        <form id="submit-bug-form">
-            <input type="text" name="title" placeholder="Title" required>
-            <textarea name="comment" placeholder="Comment" required></textarea>
-            <select name="priority" required>
+        <form id="submit-bug-form" class="mt-2">
+            <input type="text" name="title" placeholder="Title" required="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <textarea name="comment" placeholder="Comment" required="" class="p-2.5 w-500 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style="position: relative;top: 15px;"></textarea>
+            <select name="priority" required="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option value="3">Low</option>
                 <option value="2">Medium</option>
                 <option value="1">High</option>
